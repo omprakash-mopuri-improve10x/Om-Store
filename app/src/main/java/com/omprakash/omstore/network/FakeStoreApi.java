0bab78f36b56fcf2aec1a8d31fce63609fcbae4a
Omprakash <omprakash.mopuri@improve10x.com>
2023-06-02 14:21:12 +0530

    Implemented Logging Retrofit functionality

--- app/src/main/java/com/omprakash/omstore/network/FakeStoreApi.java
+++ app/src/main/java/com/omprakash/omstore/network/FakeStoreApi.java
@@ -2,15 +2,25 @@ package com.omprakash.omstore.network;
 
 import com.omprakash.omstore.Constants;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FakeStoreApi {
 
     public FakeStoreService createCategoryService() {
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Constants.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
         FakeStoreService fakeStoreService = retrofit.create(FakeStoreService.class);
         return fakeStoreService;
